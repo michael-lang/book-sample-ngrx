341c82a88c12600fc7cc6461e0938c13ad60b007
Michael Lang <michael.lang@eagletg.com>
2018-07-31 11:19:56 -0500

    import / refactor book feature selectors from ngrx platform example

--- src/app/books/store/book.store.selectors.ts
+++ src/app/books/store/book.store.selectors.ts
@@ -1,4 +1,6 @@
-import { BookState } from './book.store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { BookState, BookFeatureState, bookAdapter } from './book.store';
+
 
 /**
  * Because the data structure is defined within the reducer it is optimal to
@@ -18,3 +20,84 @@ export const getQuery = (state: BookState) => state.query;
 export const getLoading = (state: BookState) => state.loading;
 
 export const getError = (state: BookState) => state.error;
+
+/**
+ * The createFeatureSelector function selects a piece of state from the root of the state object.
+ * This is used for selecting feature states that are loaded eagerly or lazily.
+ */
+export const selectBooksFeatureState = createFeatureSelector<BookFeatureState>('books');
+
+/**
+ * Every reducer module exports selector functions, however child reducers
+ * have no knowledge of the overall state tree. To make them usable, we
+ * need to make new selectors that wrap them.
+ *
+ * The createSelector function creates very efficient selectors that are memoized and
+ * only recompute when arguments change. The created selectors can also be composed
+ * together to select different pieces of state.
+ */
+export const selectBookState = createSelector(
+  selectBooksFeatureState,
+  state => state.books
+);
+
+export const selectSelectedBookId = createSelector(
+  selectBookState,
+  getSelectedId
+);
+
+/**
+ * Adapters created with @ngrx/entity generate
+ * commonly used selector functions including
+ * getting all ids in the record set, a dictionary
+ * of the records by id, an array of records and
+ * the total number of records. This reduces boilerplate
+ * in selecting records from the entity state.
+ */
+export const {
+  selectIds: getBookIds,
+  selectEntities: getBookEntities,
+  selectAll: getAllBooks,
+  selectTotal: getTotalBooks,
+} = bookAdapter.getSelectors(selectBookState);
+
+export const selectSelectedBook = createSelector(
+  getBookEntities,
+  selectSelectedBookId,
+  (entities, selectedId) => {
+    return selectedId && entities[selectedId];
+  }
+);
+
+/**
+ * Just like with the books selectors, we also have to compose the search
+ * reducer's and collection reducer's selectors.
+ */
+export const selectSearchBookIds = createSelector(
+  selectBookState,
+  getIds
+);
+export const selectSearchQuery = createSelector(
+  selectBookState,
+  getQuery
+);
+export const selectSearchLoading = createSelector(
+  selectBookState,
+  getLoading
+);
+export const selectSearchError = createSelector(
+  selectBookState,
+  getError
+);
+
+/**
+ * Some selector functions create joins across parts of state. This selector
+ * composes the search result IDs to return an array of books in the store.
+ */
+export const getSearchResults = createSelector(
+  getBookEntities,
+  selectSearchBookIds,
+  (books, searchIds) => {
+    return (<string[]>searchIds).map(id => books[id]);
+  }
+);
--- src/app/books/store/collection.store.selectors.ts
+++ src/app/books/store/collection.store.selectors.ts
@@ -1,4 +1,7 @@
+import { createSelector } from '@ngrx/store';
 import { CollectionState } from './collection.store';
+import { selectBooksFeatureState, getBookEntities, selectSelectedBookId } from './book.store.selectors';
+import { BookFeatureState } from './book.store';
 
 
 export const getLoaded = (state: CollectionState) => state.loaded;
@@ -7,4 +10,36 @@ export const getLoading = (state: CollectionState) => state.loading;
 
 export const getIds = (state: CollectionState) => state.ids;
 
+export const selectCollectionState = createSelector(
+  selectBooksFeatureState,
+  (state: BookFeatureState) => state.collection
+);
 
+export const selectCollectionLoaded = createSelector(
+  selectCollectionState,
+  getLoaded
+);
+export const selectCollectionLoading = createSelector(
+  selectCollectionState,
+  getLoading
+);
+export const selectCollectionBookIds = createSelector(
+  selectCollectionState,
+  getIds
+);
+
+export const getBookCollection = createSelector(
+  getBookEntities,
+  selectCollectionBookIds,
+  (entities, ids) => {
+    return ids.map(id => entities[id]);
+  }
+);
+
+export const isSelectedBookInCollection = createSelector(
+  selectCollectionBookIds,
+  selectSelectedBookId,
+  (ids, selected) => {
+    return ids.indexOf(selected) > -1;
+  }
+);
